8c7bd4557962cef7a6daf037d4c2db810d5f266e
Manas Prakash <manasp2004@gmail.com>
2020-11-09 16:32:45 +0530

    Project Finished

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 # Game URL: https://www.agame.com/game/magic-piano-tiles
 
 # X Position of Tiles:
-# Tile 1: 536
+# Tile 1: 520
 # Tile 2: 629
 # Tile 3: 706
-# Tile 4: 797
+# Tile 4: 805
 
-# Y Position of all Tiles: 400
+# Y Position of all Tiles: 300
 
 # ------------------------------------------------------------------------- #
 
 import pyautogui
 import time
 import keyboard
+import win32api, win32con
+
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0)
+    time.sleep(0.009)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
 
 pyautogui.FAILSAFE = False
 
+time.sleep(1)
+
 while True:
     if keyboard.is_pressed('q') == False:
-        if pyautogui.pixel(536, 400)[0] == 0:
-            pyautogui.click(536, 400, button='right')
+        if pyautogui.pixel(520, 300)[0] == 0:
+            click(520, 305)
 
-        elif pyautogui.pixel(629, 400)[0] == 0:
-            pyautogui.click(629, 400, button='right')
+        elif pyautogui.pixel(629, 300)[0] == 0:
+            click(629, 305)
 
-        elif pyautogui.pixel(706, 400)[0] == 0:
-            pyautogui.click(706, 400, button='right')
+        elif pyautogui.pixel(706, 300)[0] == 0:
+            click(706, 305)
 
-        elif pyautogui.pixel(797, 400)[0] == 0:
-            pyautogui.click(797, 400, button='right')
+        elif pyautogui.pixel(805, 300)[0] == 0:
+            click(805, 305)
     else:
         print("Script Aborted!")
         break
