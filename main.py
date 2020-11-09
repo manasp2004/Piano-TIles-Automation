906f3641954ce6819fd969e0cace28e486dc5bb8
Manas Prakash <manasp2004@gmail.com>
2020-11-09 11:50:24 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Game URL: https://www.agame.com/game/magic-piano-tiles
+
+# X Position of Tiles:
+# Tile 1: 536
+# Tile 2: 629
+# Tile 3: 706
+# Tile 4: 797
+
+# Y Position of all Tiles: 430
+
+# ------------------------------------------------------------------------- #
